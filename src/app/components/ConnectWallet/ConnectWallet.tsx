820369e6e640fb907446b07d52e7e4d1fdc45355
Eiman <eabdelmoneim@gmail.com>
2024-09-21 19:03:09 -0500

    moved ecosystem info to .env files

--- src/app/components/ConnectWallet/ConnectWallet.tsx
+++ src/app/components/ConnectWallet/ConnectWallet.tsx
@@ -5,9 +5,12 @@ import { ecosystemWallet, createWallet } from "thirdweb/wallets";
 import { arbitrumSepolia } from "thirdweb/chains";
 import { client } from "../../utils/thirdwebClient";
 
+const ecosystemWalletName = process.env.NEXT_PUBLIC_ECOSYSTEM_WALLET_NAME || "";
+const partnerId = process.env.NEXT_PUBLIC_PARTNER_ID || "";
+
 const wallets = [
-  ecosystemWallet("ecosystem.treasure-chest-ecosystem", {
-    partnerId: "1415d24e-c7b0-4fce-846e-740841ef2c32"
+  ecosystemWallet(`ecosystem.${ecosystemWalletName}`, {
+    partnerId: partnerId
   }),
   createWallet("io.metamask"),
   createWallet("com.coinbase.wallet"),
